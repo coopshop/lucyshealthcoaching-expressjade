a265b1df5dd38752e412e3e439e6905fe292c45e
Todd Morningstar <techarchist@gmail.com>
2018-01-16 16:42:03 -0500

    Create About view
     * Allow "/index" or "/" for home page

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get(/\/(index)?/, function(req, res, next) {
   res.render('index'/*, { title: 'Express' }*/);
+});
+
+/* GET About page */
+router.get('/about', function(req, res, next) {
+  res.render('about');
 });
 
 module.exports = router;
