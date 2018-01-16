dbbe5c3202f2638bc032c7e72f5211cef59be3d2
Todd Morningstar <techarchist@gmail.com>
2018-01-16 16:49:46 -0500

    Add Work-With-Me page

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get(/\/(index)?/, function(req, res, next) {
+router.get(['/', '/index'], function(req, res, next) {
   res.render('index'/*, { title: 'Express' }*/);
 });
 
 /* GET About page */
 router.get('/about', function(req, res, next) {
   res.render('about');
+});
+
+/* GET Wortk With Me page */
+router.get('/work', function(req, res, next) {
+  res.render('work');
 });
 
 module.exports = router;
