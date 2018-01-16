1c6c3d58b664b49eeed39bc5d86648da42e9081b
Todd Morningstar <techarchist@gmail.com>
2018-01-16 17:43:55 -0500

    Add Contact, Media, and Health History pages

--- routes/index.js
+++ routes/index.js
@@ -11,8 +11,28 @@ router.get('/about', function(req, res, next) {
   res.render('about');
 });
 
-/* GET Praise page */
+/* GET Contact page */
+router.get('/contact', function(req, res, next) {
+  res.render('contact');
+});
 
+/* GET Health History pages */
+router.get('/mens-health-history-form', function(req, res, next) {
+  res.render('mens_health_history_form');
+});
+router.get('/womens-health-history-form', function(req, res, next) {
+  res.render('womens_health_history_form');
+});
+router.get('/gender_neutral-health-history-form', function(req, res, next) {
+  res.render('gender_neutral_health_history_form');
+});
+
+/* GET Media page */
+router.get('/media', function(req, res, next) {
+  res.render('media');
+});
+
+/* GET Praise page */
 router.get('/praise', function(req, res, next) {
   res.render('praise');
 });
